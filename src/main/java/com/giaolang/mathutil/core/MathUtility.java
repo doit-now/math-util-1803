94d55f26c20d0f054b378ec14ba8b7c870f165f4
doit-now <hoang.nguyenthe@gmail.com>
2025-03-12 11:40:08 +0700

    update getfactorial method() to use recursion

--- src/main/java/com/giaolang/mathutil/core/MathUtility.java
+++ src/main/java/com/giaolang/mathutil/core/MathUtility.java
@@ -11,6 +11,9 @@ public class MathUtility {
     //20! vừa đủ kiểu long, 21! kiểu long chứa ko nổi, tràn
     //quy ước hàm này chỉ tính n từ 0...20
     //code hàm thì cx đồng thời phải kiểm thử hàm; muốn kiểm thử phải có test case!!!
+
+    //CHUYỂN SANG TÍNH GIAI THỪA BẰNG ĐỆ QUY, GỌI LẠI CHÍNH MÌNH VỚI QUY MÔ NHỎ HƠN
+    //N! = N * (N - 1)!
     public static long getFactorial(int n) {
 
         if (n < 0 || n > 20) {
@@ -18,18 +21,33 @@ public class MathUtility {
             throw new IllegalArgumentException("Invalid n. n must be between 0 and 20");
         }
 
-        if (n == 0) {
+        if (n == 0 || n == 1) {
             return 1; //early return, thoát sớm nếu có thể!!!
         }
-        // khỏi viết else cho code nhìn đẹp
-        //n bắt đầu từ 1...
-        //thuật toán con heo đất, nhân dồn, cộng dồn
-        long result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;  //i bị nhân dồn vào result = result * i;
-        }
-        return result;
+        return n * getFactorial(n - 1);
     }
+
+
+
+//    public static long getFactorial(int n) {
+//
+//        if (n < 0 || n > 20) {
+//            //return -1;
+//            throw new IllegalArgumentException("Invalid n. n must be between 0 and 20");
+//        }
+//
+//        if (n == 0) {
+//            return 1; //early return, thoát sớm nếu có thể!!!
+//        }
+//        // khỏi viết else cho code nhìn đẹp
+//        //n bắt đầu từ 1...
+//        //thuật toán con heo đất, nhân dồn, cộng dồn
+//        long result = 1;
+//        for (int i = 1; i <= n; i++) {
+//            result *= i;  //i bị nhân dồn vào result = result * i;
+//        }
+//        return result;
+//    }
 
     //> TEST CASE #1
     //> VERIFY GETFACTORIAL() WITH N = 0
